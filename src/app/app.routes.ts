d4dba1d4198ef76c6f791ce136bf7ccf97cca7c0
Galiana20 <delgaliana@gmail.com>
2024-04-02 23:07:02 +0200

    Add bostrap, components and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
+import { CreatePageComponent } from './pages/create-page/create-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: 'admin', component: AdminPageComponent },
+    { path: 'create', component: CreatePageComponent },
+    { path: '**', redirectTo: 'home' }
+];
